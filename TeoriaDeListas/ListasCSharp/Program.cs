bab8859db3038f8afa17df873e8891d982e7fd2f
Juan Fe <165521278+juanfedevmaster@users.noreply.github.com>
2025-03-07 17:55:13 -0500

    Teoria de Listas

--- TeoriaDeListas/ListasCSharp/Program.cs
+++ TeoriaDeListas/ListasCSharp/Program.cs
@@ -6,7 +6,15 @@ namespace ListasCSharp
     {
         static void Main(string[] args)
         {
-            List<int> numeros = new List<int>();
+            //Ejemplo1();
+            //Ejemplo2();
+            Ejemplo3();
+        }
+
+        #region [Introducción a listas CSharp]
+        public static void Ejemplo1()
+        {
+            var numeros = new List<int>();
 
             numeros.Add(5);
             numeros.Add(10);
@@ -46,13 +54,89 @@ namespace ListasCSharp
             var indice = numeros.IndexOf(10);
             numeros[indice] = 1000;
 
-            var actualizar = numeros.Find(x => x == 1000);
-            actualizar = 4000;
+            var actualizar = numeros.Find(n => n == 1000);
 
-            for (int i = 0; i < numeros.Count; i++)
+            if (actualizar != 0)
             {
-                Console.WriteLine(numeros[i]);
+                Console.WriteLine($"El elemento {actualizar} fue encontrado en la lista");
             }
         }
+        #endregion
+
+        #region [Manejo de Predicados para Listas]
+
+        public static void Ejemplo2()
+        {
+            List<int> numeros = new List<int>();
+
+            for (var i = 0; i < 10000; i++)
+            {
+                Random random = new Random();
+                numeros.Add(random.Next(1, 10000));
+            }
+            Console.WriteLine("La lista se lleno de forma correcta.");
+
+            Console.WriteLine("Ingrese el número a buscar: ");
+            int numeroCliente = Int32.Parse(Console.ReadLine());
+
+            if (numeros.Exists(x => x == numeroCliente))
+            {
+                Console.WriteLine($"El número {numeroCliente} fue encontrado en la lista.");
+            }
+            else
+            {
+                Console.WriteLine($"El número {numeroCliente} no fue encontrado en la lista.");
+            }
+
+            List<int> numerosFiltrados = new List<int>();
+
+            numerosFiltrados = numeros.FindAll(n => n > numeroCliente && n < (numeroCliente + 3000));
+            Console.WriteLine($"Se encontraron {numerosFiltrados.Count} números mayores a {numeroCliente} y menores a {numeroCliente + 3000}");
+
+        }
+
+        #endregion
+
+        #region [Manejo de Predicados para Listas Parte 2]
+
+        public static void Ejemplo3()
+        {
+            List<string> nombres = new List<string>
+            {
+                "Juan", "Carlos", "José", "Luis", "Pedro", "Miguel", "Francisco", "Andrés", "Javier", "Fernando",
+                "Alejandro", "Manuel", "Ricardo", "Hugo", "Diego", "Eduardo", "Gabriel", "Raúl", "David", "Antonio",
+                "Ángel", "Salvador", "Sergio", "Rodrigo", "Emilio", "Martín", "Ernesto", "Roberto", "Óscar", "Esteban",
+                "Iván", "Joaquín", "Gonzalo", "Alberto", "Rafael", "Tomás", "Guillermo", "Bruno", "Samuel", "Cristian",
+                "Julio", "Vicente", "Emanuel", "Nicolás", "Lucas", "Héctor", "Adrián", "Simón", "Kevin", "Fabián"
+            };
+
+            List<string> nombresCompuestos = new List<string>();
+
+            //for (int i = 0; i < nombres.Count; i++)
+            //{
+            //    for (int j = 0; j < nombres.Count; j++)
+            //    {
+            //        if (i != j)
+            //        {
+            //            nombresCompuestos.Add(nombres[i] + " " + nombres[j]);
+            //        }
+            //    }
+            //}
+
+            nombresCompuestos = nombres.SelectMany((nombre1, index1) => nombres
+                .Skip(index1 + 1)
+                .Select(nombre2 => nombre1 + " " + nombre2)
+            ).ToList();
+
+            foreach (var nombre in nombresCompuestos)
+            {
+                Console.WriteLine(nombre);
+            }
+
+        }
+
+
+
+        #endregion
     }
 }
